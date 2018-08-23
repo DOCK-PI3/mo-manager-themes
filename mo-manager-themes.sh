94e9f46ca5ac2117275afacf1de7d3dbf5af4852
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2018-08-23 23:52:17 +0200

    update
    url de descarga nuevas

--- mo-manager-themes.sh
+++ mo-manager-themes.sh
@@ -44,8 +44,8 @@ function main_menu() {
 function manager_install_blueandred() {                                          #
 dialog --infobox "...Instalando Manager theme Blue and Red..." 25 55 ; sleep 3
 cd /opt/masos/supplementary/retropie-manager/project/templates/
-  sudo rm skeleton.html
-	wget https://doc-04-00-docs.googleusercontent.com/docs/securesc/d1ibgdk8cmi2teg1rrg47ua4rt6te0bs/ru9m0qqp5h8mu1rgrhsk6c92m7upr8kv/1535047200000/10946459356008990870/10946459356008990870/1di_-3UUfcy4c6rUHjjzin8w5nGDDa1Th?e=download&nonce=007ftku38bu3c&user=10946459356008990870&hash=j17kjn1qt422vp2u6rqv5eo32jiii1ue
+ # sudo rm skeleton.html
+ wget http://masos.ga/?download=158
 dialog --infobox "...Manager theme Blue and Red se instalo correctamente!..." 25 55 ; sleep 5 
 }
 
@@ -55,8 +55,8 @@ dialog --infobox "...Manager theme Blue and Red se instalo correctamente!..." 25
 function manager_install_oscuridad() {                                          
 dialog --infobox "...Instalando Manager theme Oscuridad..." 25 55 ; sleep 5
 cd /opt/masos/supplementary/retropie-manager/project/templates/
-  sudo rm skeleton.html
-  wget https://doc-0c-00-docs.googleusercontent.com/docs/securesc/d1ibgdk8cmi2teg1rrg47ua4rt6te0bs/bnvvtnce5v70tvqg3t5e6qsaid3bitk4/1535061600000/10946459356008990870/10946459356008990870/1-sNDxg5duftk7xu82md-vigQ9imLYF1J?e=download
+  # sudo rm skeleton.html
+  wget http://masos.ga/?download=162
 dialog --infobox "...Manager theme Oscuridad se instalo correctamente!..." 25 55 ; sleep 10
 # ---------------- FIN DEL CODIGO ------------ #
 }
